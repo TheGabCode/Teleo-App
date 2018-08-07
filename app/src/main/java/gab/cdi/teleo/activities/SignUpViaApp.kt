13e10dea4b8d494e06d4e0b32b73e729a71e2d68
TheGabCode <gabgonzales@codedisruptors.com>
2018-08-07 18:14:40 +0800

    Initial commit, Registration UI

---
+++ app/src/main/java/gab/cdi/teleo/activities/SignUpViaApp.kt
@@ -0,0 +1,13 @@
+package gab.cdi.teleo.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import gab.cdi.teleo.R
+
+class SignUpViaApp : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_via_app)
+    }
+}
